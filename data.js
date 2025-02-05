b9cf41cb4809ba8ff50dfea6cb7c895e7ac03f04
nerfis-code <nerfisalcantara2017@gmail.com>
2025-02-05 10:47:14 -0400

    Typing Practice

---
+++ data.js
@@ -0,0 +1,28 @@
+export let tex = [
+    "música", "cálido", "sofá", "público", "fácil", "cámara", "médico", "lápiz", "túnel", "vehículo",
+    "avión", "cómic", "dócil", "fórmula", "débil", "cráter", "régimen", "árbol", "príncipe", "góndola",
+    "tránsito", "nación", "máquina", "póster", "ópera", "héroe", "pésimo", "sándwich", "tóxico", "júbilo",
+    "época", "política", "típico", "cálculo", "práctica", "público", "brújula", "fósforo", "místico", "pájaro",
+    "teléfono", "lógica", "álgebra", "cómputo", "mármol", "rítmico", "médula", "símil", "fútbol", "estética",
+    "oxígeno", "espátula", "crítico", "cómodo", "vórtice", "tómbola", "múltiple", "álbum", "género", "lánguido",
+    "ánimo", "óvalo", "tópico", "sílaba", "mínimo", "dúo", "diámetro", "óptico", "antídoto", "fértil",
+    "hábito", "ídolo", "clímax", "mágico", "nómada", "término", "tétrico", "tópico", "ídem", "óxido",
+    
+    // Palabras difíciles
+    "obnubilado", "recalcitrante", "inexorable", "perplejidad", "esotérico", "cónclave", "ataraxia",
+    "idiosincrasia", "volubilidad", "nefelibata", "ignominia", "dipsomanía", "exégesis", "misántropo",
+    "pusilánime", "onomástica", "estulticia", "hierático", "panegírico", "acrimonia", "coadyuvar",
+    "acendrado", "efluvio", "ignoto", "inefable", "perspicaz", "irredento", "probóscide", "troglodita",
+    "metempsicosis", "ubérrimo", "inequívoco", "heurístico", "apócrifo", "contumaz", "altruismo",
+    "demagogia", "procrastinar", "eclecticismo", "lóbrego", "fatuo", "perentorio", "tahúr", "venático",
+    "exangüe", "protervo", "epónimo", "nefelibata", "ignívomo", "perspicuo", "anacoluto", "díscolo",
+    "sagaz", "obsequioso", "cicatero", "oropel", "pétreo", "reprobo", "acrónimo", "xenofobia", "flagelo",
+    "claudicar", "sibarita", "venal", "epístola", "atavismo", "jactancia", "nihilismo", "quimera",
+    "cáfila", "dicotomía", "exacerbado", "acrítico", "antropomorfismo", "incólume", "ominoso",
+    "hierofante", "periplo", "solipsismo", "usufructo", "melifluo", "obcecado", "ampuloso", "semiótica",
+    "numismático", "consuetudinario", "hórrido", "petulante", "perífrasis", "inanición", "condigno",
+    "exacerbación", "inextricable", "endogamia", "apología", "coadyuvante", "impúdico", "lenidad",
+    "paroxismo", "redargüir", "solipsismo", "subrepticio", "uxoricidio", "vaguedad", "xerófilo",
+    "inmarcesible", "infalibilidad", "mucílago", "paralogismo", "soturno", "yerro", "hiperbóreo",
+    "intersticio", "tremolante", "vicisitud", "teratológico", "inexorable", "imponderable"
+];
---
+++ script.js
@@ -0,0 +1,110 @@
+import {tex} from "./data.js"
+
+const $paragraph = document.querySelector("p")
+const $reload = document.querySelector("button")
+const $wpm = document.querySelector("h2.wpm")
+const $percent = document.querySelector("h2.percent")
+
+const $imageEnter = document.createElement("img")
+$imageEnter.setAttribute("src","image/Backspace.png")
+$imageEnter.classList.add("enter")
+$imageEnter.addEventListener("click", ()=>{
+    let custom = new Event("keypress")
+    custom.key = "Enter"
+    on_press(custom)
+});
+
+
+window.addEventListener("focus", on_focus);
+window.addEventListener("blur", on_blur);
+document.addEventListener("keypress",on_press)
+$reload.addEventListener("click",init_game)
+
+let currWord 
+let currLetter
+let currTime = 0
+let interval = null
+let failedTheFirstAttempt = false
+let press_enter = false
+
+
+function init_game(){
+    $reload.blur()
+    $paragraph.innerHTML = ""
+    $wpm.innerHTML = ""
+    $percent.innerHTML = ""
+    clearInterval(interval)
+    failedTheFirstAttempt = false
+
+    const texto = tex.toSorted(()=> {return Math.random() - .5}).slice(0,24)
+    for(const word of texto){
+        let x_word = document.createElement("word")
+        word.split("").map((letter)=>{
+            let x_letter = document.createElement("letter")
+            x_letter.textContent = letter
+            x_word.appendChild(x_letter)
+        })
+        $paragraph.appendChild(x_word)
+    
+        let x_void = document.createElement("void")
+        let x_space = document.createElement("space")
+        x_space.textContent = " "
+        x_void.appendChild(x_space)
+        $paragraph.appendChild(x_void)
+    }
+    currWord = document.querySelector("word")
+    currLetter = document.querySelector("letter")
+    currLetter.classList.add("active")
+}
+init_game()
+
+function end_game(){
+    clearInterval(interval)
+    $wpm.textContent = (24 / (currTime / 60)).toFixed(2)  + " wpm"
+    let corrects = [...document.querySelectorAll("letter.correct")].length;
+    let incorrects = [...document.querySelectorAll("letter.incorrect")].length;
+    $percent.textContent = (100 - ((incorrects/corrects)*100)).toFixed(2) + " %"
+}
+function on_focus(){}
+function on_blur(){}
+
+function on_press(event){
+    
+    if (interval == null){
+        interval = setInterval(()=>currTime++,1000)
+    }
+    
+    if (!press_enter && event.key != currLetter.textContent) {
+        failedTheFirstAttempt = true
+        return
+    }
+    if (press_enter){
+        if (event.key != "Enter"){
+            return
+        }
+        press_enter = false
+        currLetter.removeChild($imageEnter)
+    }
+    if (failedTheFirstAttempt) currLetter.classList.add("incorrect")
+    else currLetter.classList.add("correct")
+
+    failedTheFirstAttempt = false
+    currLetter.classList.remove("active")
+    let next = currLetter.nextElementSibling
+
+    if (next == null){
+        currWord = currWord.nextElementSibling
+        if (currWord == null){
+            end_game()
+            return
+        }
+        next = currWord.firstChild
+        if(next.offsetWidth == 0){
+            next.appendChild($imageEnter)
+            press_enter = true
+        }
+    }
+    next.classList.add("active")
+    currLetter = next
+}
+
